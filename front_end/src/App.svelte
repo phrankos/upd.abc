faa061317042e31500dd6c26b4dc91dcc99de326
phrankos <rmcagulada@up.edu.ph>
2024-03-18 15:07:17 +0800

    minor changes

--- front_end/src/App.svelte
+++ front_end/src/App.svelte
@@ -10,6 +10,7 @@ Bord is a message board for bored people with money.
 
 Prerequisites: Being bored, money (ETH).
 
+Step 0: Connect Metamask Wallet.
 Step 1: Read Bord.
 Step 2: If you want to change the message on the Bord, proceed to Step 3, otherwise go to Step 6.
 Step 3: Click the Edit Button and write your message on the Bord.
@@ -21,9 +22,8 @@ Bord increments the Price of posting a message every submission.
 You can see the number of submissions on the bottom-left corner.
 You can see the Current Price at the bottom-right corner.
 
-May you become less bored by reading the board named Bord :D
-
 Click the Bord to load the current message!
+May you become less bored by reading the board named Bord :D
 
 *Bord is not liable for anyone, anything, anytime, anywhere, anyway, for any reason.*`;
 
@@ -48,7 +48,7 @@ Click the Bord to load the current message!
     numSubmissions = await contract.numberSubmissions();
     displayPrice = Number(BigInt(price) / 10n**9n) / 10**9;
   };
-  
+
   const readMsg = async () => {
     const { ethereum } = window as any;
     const provider = new ethers.BrowserProvider(ethereum);
